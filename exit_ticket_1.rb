65c87370cb8b74e804301e8e01b856f86ad32cbb
Richard Rosa Serrano <richardrosaserrano@gmail.com>
2018-04-24 22:09:07 -0600

    the bonus section is done

--- exit_ticket_1.rb
+++ exit_ticket_1.rb
@@ -9,12 +9,15 @@ things_i_like = ["Tesla Model S", "Tesla Model 3", "Tesla Model X", "Ford Model 
  things_i_like.insert(2, "Tesla Model Roadseter")
 # Remove the first element from your array
 things_i_like.delete_at(0)
-things_i_like
 # Remove any duplicates that may exist in your array
  things_i_like.uniq
 # Create a second array with 3 elements
 things_i_hate = ["chicago style pizza", "rasion cookies", "Richard"]
 # "Sqash" the two arrays together
 
+things_i_hate_and_love = things_i_hate + things_i_like
+puts things_i_hate_and_love
 # BONUS since we didn't cover today 🦄
+var = things_i_like.join(", ")
+puts "this is a list of all the things that I love: #{var}"
 # If your array contains strings, write a sentence using each string. If it contains integers, create a multiplication table for your array.
